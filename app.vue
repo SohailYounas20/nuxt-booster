0f9bc676dbfdcbb98e736146a77a2af331230fb3
SohailYounas20 <sohail.younas@ki5.co.uk>
2024-09-09 19:48:56 +0500

    deploying on vercel

--- app.vue
+++ app.vue
@@ -1,12 +1,14 @@
 <template>
   <div>{{ text }}</div>
-  <button @click="checking">click me</button>
+  <button @click.native="checking">click me</button>
 </template>
 
 <script lang="ts" setup>
 const text = ref("this is testing project for nuxt booster");
 const checking = () => {
-  console.log("checking");
-  text.value = "button is clicked";
+  nextTick(() => {
+    console.log("checkingdsf");
+    text.value = "button is clicked";
+  });
 };
 </script>
