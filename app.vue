d81e0cad1620d2e5499ecaf73c5c9a491d82ebea
SohailYounas20 <sohail.younas@ki5.co.uk>
2024-09-09 18:29:03 +0500

    deploying on vercel

--- app.vue
+++ app.vue
@@ -1,3 +1,10 @@
 <template>
   <div>this is testing project for nuxt booster</div>
+  <button @click="checking">click me</button>
 </template>
+
+<script lang="ts" setup>
+const checking = () => {
+  console.log("checking");
+};
+</script>
