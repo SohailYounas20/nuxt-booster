3d21dd1dca890d83c75ba5d6170b7ebeb796548b
SohailYounas20 <sohail.younas@ki5.co.uk>
2024-09-09 18:32:59 +0500

    deploying on vercel

--- app.vue
+++ app.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>this is testing project for nuxt booster</div>
+  <div>{{ text }}</div>
   <button @click="checking">click me</button>
 </template>
 
 <script lang="ts" setup>
+const text = ref("this is testing project for nuxt booster");
 const checking = () => {
   console.log("checking");
+  text.value = "button is clicked";
 };
 </script>
